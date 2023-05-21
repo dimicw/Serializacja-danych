8ad6c9f47fd87aa5e47ce0a391f28a25f7fb479a
dimicw <104204285+dimicw@users.noreply.github.com>
2023-05-21 21:28:40 +0200

    Initial commit

---
+++ Zadanie2_DCW/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Zadanie2_DCW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serializeButton = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbSurname = new System.Windows.Forms.TextBox();
+            this.numericAge = new System.Windows.Forms.NumericUpDown();
+            this.deserializeButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericAge)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serializeButton
+            // 
+            this.serializeButton.Location = new System.Drawing.Point(14, 91);
+            this.serializeButton.Name = "serializeButton";
+            this.serializeButton.Size = new System.Drawing.Size(112, 23);
+            this.serializeButton.TabIndex = 0;
+            this.serializeButton.Text = "Serializuj";
+            this.serializeButton.UseVisualStyleBackColor = true;
+            this.serializeButton.Click += new System.EventHandler(this.serializeButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(71, 12);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(172, 20);
+            this.tbName.TabIndex = 1;
+            // 
+            // tbSurname
+            // 
+            this.tbSurname.Location = new System.Drawing.Point(71, 38);
+            this.tbSurname.Name = "tbSurname";
+            this.tbSurname.Size = new System.Drawing.Size(172, 20);
+            this.tbSurname.TabIndex = 2;
+            // 
+            // numericAge
+            // 
+            this.numericAge.Location = new System.Drawing.Point(71, 65);
+            this.numericAge.Minimum = new decimal(new int[] {
+            18,
+            0,
+            0,
+            0});
+            this.numericAge.Name = "numericAge";
+            this.numericAge.Size = new System.Drawing.Size(172, 20);
+            this.numericAge.TabIndex = 3;
+            this.numericAge.Value = new decimal(new int[] {
+            18,
+            0,
+            0,
+            0});
+            // 
+            // deserializeButton
+            // 
+            this.deserializeButton.Location = new System.Drawing.Point(131, 91);
+            this.deserializeButton.Name = "deserializeButton";
+            this.deserializeButton.Size = new System.Drawing.Size(112, 23);
+            this.deserializeButton.TabIndex = 4;
+            this.deserializeButton.Text = "Deserializuj";
+            this.deserializeButton.UseVisualStyleBackColor = true;
+            this.deserializeButton.Click += new System.EventHandler(this.deserializeButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "imię: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "nazwisko:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "wiek: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(259, 137);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.deserializeButton);
+            this.Controls.Add(this.numericAge);
+            this.Controls.Add(this.tbSurname);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.serializeButton);
+            this.Name = "Form1";
+            this.Text = "Zadanie 2 - DCW";
+            ((System.ComponentModel.ISupportInitialize)(this.numericAge)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button serializeButton;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbSurname;
+        private System.Windows.Forms.NumericUpDown numericAge;
+        private System.Windows.Forms.Button deserializeButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Zadanie2_DCW/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Windows.Forms;
+
+namespace Zadanie2_DCW
+{
+    public partial class Form1 : Form
+    {
+        //klasa do zserializowania
+        [Serializable] 
+        public class Client
+        {
+            public string name;
+            public string surname;
+            public int age;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void serializeButton_Click(object sender, EventArgs e)
+        {
+            //obiekt do serializacji
+            Client client = new Client();
+
+            //okno zapisu do pliku
+            saveFileDialog1.Filter = "Pliki binarne (*.dat)|*.dat";
+            saveFileDialog1.Title = "Zapisz do pliku";
+            
+            if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                FileStream fileStream = new FileStream(saveFileDialog1.FileName, FileMode.Create);
+                BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+                try
+                {
+                    //pobranie danych z pól
+                    client.name = tbName.Text;
+                    client.surname = tbSurname.Text;
+                    client.age = (int)numericAge.Value;
+
+                    //serializacja
+                    binaryFormatter.Serialize(fileStream, client);
+
+                    MessageBox.Show("Obiekt został zserializowany", "Zapisano");
+                }
+                catch (SerializationException ex)
+                {
+                    MessageBox.Show("Wystąpił błąd przy serializacji\n" + ex.Message, "Błąd");
+                }
+                finally
+                {
+                    fileStream.Close();
+                }
+            }
+        }
+
+        private void deserializeButton_Click(object sender, EventArgs e)
+        {
+            //obiekt do zapisu po deserializacji
+            Client client = new Client();
+
+            //okno wyboru pliku do wczytania
+            openFileDialog1.Filter = "Pliki binarne (*.dat)|*.dat";
+            openFileDialog1.Title = "Otworz plik";
+
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                FileStream fileStream = new FileStream(openFileDialog1.FileName, FileMode.Open);
+                BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+                try
+                {
+                    //deserializacja
+                    client = (Client)binaryFormatter.Deserialize(fileStream);
+
+                    //uzupełnienie pól danymi z deserializacji
+                    tbName.Text = client.name;
+                    tbSurname.Text = client.surname;
+                    numericAge.Value = client.age;
+
+                    MessageBox.Show("Obiekt został zdeserializowany", "Wczytano");
+                }
+                catch (SerializationException ex)
+                {
+                    MessageBox.Show("Wystąpił błąd przy deserializacji\n" + ex.Message, "Błąd");
+                }
+                finally
+                {
+                    fileStream.Close();
+                }
+            }
+        }
+    }
+}
